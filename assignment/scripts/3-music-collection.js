f8f00971a5e0d3ef26bcdcf5ef0561763a23ee1a
Mark Schumacher <schuma1022@gmail.com>
2022-06-30 11:51:03 -0500

    Update function and add tracks to firt album

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,14 +13,16 @@ const sadCollection = [{title: 'test title', artist: 'test artist', year: 2022}]
  * @param {string} title - is for the title of the album
  * @param {string} artist - is for the name of the artist
  * @param {number} yearPublished - is for the year the album was published
+ * @param {object[]} tracks - is for an array of tracks with name and duration properties
  * 
  * @returns - the newAlbum object
  */
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, artist, yearPublished, tracks) {
     const newAlbum = {
         title: title,
         artist: artist,
         year: yearPublished,
+        tracks: tracks,
     };
     collection.push(newAlbum);
     return newAlbum;
@@ -29,7 +31,7 @@ function addToCollection(title, artist, yearPublished) {
 // console.log(addToCollection('test title', 'test artist', 2022));
 
 // wanted at least one 'self titled' album
-console.log('Added to collection', addToCollection('Foo Fighters', 'Foo Fighters', 1995));
+console.log('Added to collection', addToCollection('Foo Fighters', 'Foo Fighters', 1995, [{name: 'This Is a Call', duration: 3.53},{name: 'I\'ll Stick Around', duration: 3.53},{name: 'Big Me', duration: 2.12},{name: 'Alone + Easy Target', duration: 4.05},{name:'Good Grief', duration: 4.01},{name:'Floaty',duration:4.30},{name:'Weenie Beenie', duration:2.45},{name:'Oh, George', duration:3.00},{name:'For All the Cows', duration:3.30},{name:'X-Static', duration:4.13},{name:'Wattershed', duration:2.15},{name:'Exhausted', duration:5.47}]));
 // another album by the same artist
 console.log('Added to collection', addToCollection('Wasting Light', 'Foo Fighters', 2011));
 // how about an album with the same year
@@ -103,6 +105,9 @@ console.log('looking for \'Stone Temple Pilots\'', findByArtist('Stone Temple Pi
  */
 function search(albumCollection, object) {
     const foundAll = [];
+    // object.length doesn't work, neither does if object === {}
+    // object.keys() returns the object properties as an array
+    // which we can use .length on. an empty object would be 0
     if (object === undefined || Object.keys(object).length === 0) {
         return albumCollection;
     }
