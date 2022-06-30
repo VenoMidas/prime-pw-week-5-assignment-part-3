e7afad1901b684add54b1388d8be9e842e353a08
Mark Schumacher <schuma1022@gmail.com>
2022-06-30 12:24:30 -0500

    Add tracks through album 4

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -33,11 +33,11 @@ function addToCollection(title, artist, yearPublished, tracks) {
 // wanted at least one 'self titled' album
 console.log('Added to collection', addToCollection('Foo Fighters', 'Foo Fighters', 1995, [{name: 'This Is a Call', duration: 3.53},{name: 'I\'ll Stick Around', duration: 3.53},{name: 'Big Me', duration: 2.12},{name: 'Alone + Easy Target', duration: 4.05},{name:'Good Grief', duration: 4.01},{name:'Floaty',duration:4.30},{name:'Weenie Beenie', duration:2.45},{name:'Oh, George', duration:3.00},{name:'For All the Cows', duration:3.30},{name:'X-Static', duration:4.13},{name:'Wattershed', duration:2.15},{name:'Exhausted', duration:5.47}]));
 // another album by the same artist
-console.log('Added to collection', addToCollection('Wasting Light', 'Foo Fighters', 2011));
+console.log('Added to collection', addToCollection('Wasting Light', 'Foo Fighters', 2011, [{name:'Bridge Burning', duration:4.46},{name:'Rope',duration:4.19},{name:'Dear Rosemary',duration:4.26},{name:'White Limo',duration:3.22},{name:'Arlandria',duration:4.28},{name:'These Days',duration:4.58},{name:'Back & Forth',duration:3.52},{name:'A Matter of Time',duration:4.36},{name:'Miss the Misery',duration:4.33},{name:'I Should Have Known',duration:4.15},{name:'Walk',duration:4.15}]));
 // how about an album with the same year
-console.log('Added to collection', addToCollection('Endgame', 'Rise Against', 2011));
+console.log('Added to collection', addToCollection('Endgame', 'Rise Against', 2011,[{name:'Architects',duration:3.42},{name:'Help Is on the Way',duration:3.57},{name:'Make It Stop (September\'s Children',duration:3.54},{name:'Disparity by Design',duration:3.48},{name:'Satellite',duration:3.58},{name:'Midnight Hands',duration:4.17},{name:'Survivor Guilt',duration:3.59},{name:'Broken Mirrors',duration:3.54},{name:'Wait for Me',duration:3.39},{name:'A Gentlemen\'s Coup',duration:3.46},{name:'This Is Letting Go', duration:3.41},{name:'Endgame',duration:3.24}]));
 // more Foo Fighters! RIP Taylor Hawkins
-console.log('Added to collection', addToCollection('One by One', 'Foo Fighters', 2002));
+console.log('Added to collection', addToCollection('One by One', 'Foo Fighters', 2002,[{name:'All My Life',duration:4.23},{name:'Low',duration:4.28},{name:'Have It All',duration:4.57},{name:'Times Like These',duration:4.26},{name:'Disenchanted Lullaby',duration:4.33},{name:'Tired of You',duration:5.11},{name:'Halo',duration:5.06},{name:'Lonely as You',duration:4.37},{name:'Overdrive',duration:4.30},{name:'Burn Away',duration:4.58},{name:'Come Back',duration:7.49}]));
 // album 5
 console.log('Added to collection', addToCollection('Dear Agony', 'Breaking Benjamin', 2009));
 // album 6
