0b839038375f09e63a6a55e6f64cbc04e4b97f93
Mark Schumacher <schuma1022@gmail.com>
2022-06-30 07:45:00 -0500

    Add 6 albums to collection and log

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -23,3 +23,18 @@ function addToCollection(title, artist, yearPublished) {
 };
 
 // console.log(addToCollection('test title', 'test artist', 2022));
+
+// wanted at least one 'self titled' album
+console.log('Added to collection', addToCollection('Foo Fighters', 'Foo Fighters', 1995));
+// another album by the same artist
+console.log('Added to collection', addToCollection('Wasting Light', 'Foo Fighters', 2011));
+// how about an album with the same year
+console.log('Added to collection', addToCollection('Endgame', 'Rise Against', 2011));
+// more Foo Fighters! RIP Taylor Hawkins
+console.log('Added to collection', addToCollection('One by One', 'Foo Fighters', 2002));
+// album 5
+console.log('Added to collection', addToCollection('Dear Agony', 'Breaking Benjamin', 2009));
+// album 6
+console.log('Added to collection', addToCollection('Joe\'s Garage', 'Frank Zappa', 1987));
+
+console.table(collection);
