dcd093d46a3a88deecf44c8606fa229fe9da1c79
Mark Schumacher <schuma1022@gmail.com>
2022-06-30 09:17:19 -0500

    Create and test showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,11 @@
 console.log('***** Music Collection *****')
 
+// for album collection
 const collection = [];
-
+// to test logic with an empty array
+const emptyCollection = [];
+// to test logic with only 1 item in array
+const sadCollection = [{title: 'test title', artist: 'test artist', year: 2022}];
 
 /**
  * Add a new album to the collection
@@ -37,4 +41,29 @@ console.log('Added to collection', addToCollection('Dear Agony', 'Breaking Benja
 // album 6
 console.log('Added to collection', addToCollection('Joe\'s Garage', 'Frank Zappa', 1987));
 
+// instructions say console.log, I made a table instead
+// console.log(collection);
 console.table(collection);
+
+/**
+ * Logs the number of albums and each albums information
+ * 
+ * @param {Object[]} albumCollection - is for an album collection to log
+ */
+function showCollection(albumCollection) {
+    // wanted logic for single or plural albums in collection
+    if(albumCollection.length === 0 || albumCollection.length > 1) {
+        console.log('There are', albumCollection.length, 'albums in this collection');
+    } else if(albumCollection.length === 1) {
+        console.log('There is', albumCollection.length, 'album in this collection')
+    } for(let album of albumCollection) {
+        // did this a couple ways
+        console.log(`${album.title} by ${album.artist}, published in ${album.year}`)
+        // console.log(album.title, 'by', album.artist + ', published in', album.year + '.');
+    };
+};
+
+showCollection(collection);
+// tested with and empty array and a single item array
+// showCollection(emptyCollection);
+// showCollection(sadCollection);
