fb19fcc36950d90822ee36adf093d4f74bdb64b9
Mark Schumacher <schuma1022@gmail.com>
2022-06-30 07:24:35 -0500

    Create addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,25 @@
 console.log('***** Music Collection *****')
 
 const collection = [];
+
+
+/**
+ * Add a new album to the collection
+ * 
+ * @param {string} title - is for the title of the album
+ * @param {string} artist - is for the name of the artist
+ * @param {number} yearPublished - is for the year the album was published
+ * 
+ * @returns - the newAlbum object
+ */
+function addToCollection(title, artist, yearPublished) {
+    const newAlbum = {
+        title: title,
+        artist: artist,
+        year: yearPublished,
+    };
+    collection.push(newAlbum);
+    return newAlbum;
+};
+
+// console.log(addToCollection('test title', 'test artist', 2022));
